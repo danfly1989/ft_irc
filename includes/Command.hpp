858f46eee8bb7b565e05fadd29747d2a15650284
flyndann <dannyflynn@gmx.com>
2026-08-07 11:04:40 +0200

    Sever reference as command init

--- includes/Command.hpp
+++ includes/Command.hpp
@@ -6,7 +6,7 @@
 /*   By: daflynn <daflynn@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 11:12:09 by daflynn           #+#    #+#             */
-/*   Updated: 2026/08/03 11:57:10 by daflynn          ###   ########.fr       */
+/*   Updated: 2026/08/07 10:49:52 by daflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,22 @@
 
 #include "Client.hpp"
 
+class Server;
+
 class Command{
 	private:
 		bool _auth_required;
 	
+	protected:
+		Server& _server;
+	
 	public:
 		//empty state constructor makes no sense here
+		//copy assignment is also useless in this context
 		//all commands should call Command from their
 		//respective init lists
-		Command(bool auth_required);
+		Command(bool auth_required, Server& server);
 		Command(Command const & obj);
-		Command& operator=(Command const & obj);
 		virtual ~Command();
 
 		//the entire point of a command is to be executed => interface
--- src/Command.cpp
+++ src/Command.cpp
@@ -6,31 +6,22 @@
 /*   By: daflynn <daflynn@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 11:47:17 by daflynn           #+#    #+#             */
-/*   Updated: 2026/08/03 11:56:44 by daflynn          ###   ########.fr       */
+/*   Updated: 2026/08/07 11:04:07 by daflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Command.hpp"
 
-Command::Command(bool auth_required) : _auth_required(auth_required)
+Command::Command(bool auth_required, Server & server) : _auth_required(auth_required), _server(server)
 {
 	
 }
 
-Command::Command(Command const & other) : _auth_required(other._auth_required)
+Command::Command(Command const & other) : _auth_required(other._auth_required), _server(other._server)
 {
 
 }
 
-Command & Command::operator=(Command const & other)
-{
-	if(this != &other)
-	{
-		this->_auth_required = other._auth_required;
-	}
-
-	return *this; 
-}
 
 Command::~Command()
 {
