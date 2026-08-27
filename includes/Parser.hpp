0c287a77caebccddba37e640441879099fed7d5e
flyndann <dannyflynn@gmx.com>
2026-08-27 10:50:09 +0200

    looping over command map and deleting each within destructor

--- includes/Parser.hpp
+++ includes/Parser.hpp
@@ -6,7 +6,7 @@
 /*   By: daflynn <daflynn@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/19 18:14:04 by daflynn           #+#    #+#             */
-/*   Updated: 2026/08/23 18:33:25 by daflynn          ###   ########.fr       */
+/*   Updated: 2026/08/27 09:50:38 by daflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ class Server;
 class Parser{
 	private:
 		Server & _server;
+		//each command shall be an element of the _commands map
 		std::map<std::string, Command*> _commands;
 		Parser(Parser const & other);
 		Parser & operator=(Parser const & other);
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: daflynn <daflynn@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 18:34:55 by daflynn           #+#    #+#             */
-/*   Updated: 2026/08/23 18:37:30 by daflynn          ###   ########.fr       */
+/*   Updated: 2026/08/27 10:49:35 by daflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 #include "Pass.hpp"
 
 Parser::Parser(Server & server) : _server(server)
-{
+{	
+	//initating the commands map
 	_commands["PASS"] = new Pass(true, _server);
+}
+
+//naive attempt to loop over commands in destructor.
+Parser::~Parser()
+{
+	std::map<std::string, Command*>::iterator i;
+	i = _commands.begin();
+
+	while(i != _commands.end())
+	{
+		delete i->second;
+		++i;
+	}
+	_commands.clear();
 }
