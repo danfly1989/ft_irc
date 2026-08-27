93fdec817712f72e044fbf6d9fee807335913e56
flyndann <dannyflynn@gmx.com>
2026-08-27 12:14:58 +0200

    tokenization in invoke function. voided client for safe compile

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,17 +6,19 @@
 /*   By: daflynn <daflynn@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 18:34:55 by daflynn           #+#    #+#             */
-/*   Updated: 2026/08/27 10:49:35 by daflynn          ###   ########.fr       */
+/*   Updated: 2026/08/27 12:14:24 by daflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Parser.hpp"
 #include "Pass.hpp"
+#include <sstream>
+#include <vector>
 
 Parser::Parser(Server & server) : _server(server)
 {	
 	//initating the commands map
-	_commands["PASS"] = new Pass(true, _server);
+	_commands["PASS"] = new Pass(false, _server);
 }
 
 //naive attempt to loop over commands in destructor.
@@ -31,4 +33,16 @@ Parser::~Parser()
 		++i;
 	}
 	_commands.clear();
+}
+
+void	Parser::invoke(Client * client, const std::string & line)
+{
+	(void) client;
+	std::istringstream iss(line);
+	std::string word;
+	std::vector<std::string> tokens;
+	while(iss >> word)
+	{
+		tokens.push_back(word);
+	}
 }
