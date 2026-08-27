7238b7f5268fb4fb676772ceea729e8dab13c5fb
flyndann <dannyflynn@gmx.com>
2026-08-27 13:17:40 +0200

    transforming command from map into uppercase for parsing

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,7 +6,7 @@
 /*   By: daflynn <daflynn@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 18:34:55 by daflynn           #+#    #+#             */
-/*   Updated: 2026/08/27 13:06:24 by daflynn          ###   ########.fr       */
+/*   Updated: 2026/08/27 13:16:53 by daflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include "Pass.hpp"
 #include <sstream>
 #include <vector>
+#include <algorithm>
+#include <cctype>
 
 Parser::Parser(Server & server) : _server(server)
 {	
@@ -47,4 +49,10 @@ void	Parser::invoke(Client * client, const std::string & line)
 	}
 	if(tokens.empty())
 		return;
+	//commands are usually expected to be upper case
+	//not enforced by RFC but most common practice
+	//It also is more true to that 2000s IRC feel
+	std::string upper;
+	upper.resize(tokens[0].size());
+	std::transform(tokens[0].begin(), tokens[0].end(), upper.begin(), ::toupper);
 }
